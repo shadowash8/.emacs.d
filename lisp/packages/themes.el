2a548dc1eb337de31fd8980907f93b0464f1ffc6
A S H <81229115+ashwinjadhav818@users.noreply.github.com>
2026-01-13 19:21:00 +0530

    make theme auto switch between black and matugen

--- lisp/packages/themes.el
+++ lisp/packages/themes.el
@@ -1,16 +1,12 @@
 ;;; themes.el --- Theme configuration -*- lexical-binding: t; -*-
-
 ;;; Commentary:
 ;; Theme setup for Emacs
-
 ;;; Code:
-
 ;; Doom Themes
 (use-package doom-themes
   :ensure t
   :config
 ;  (load-theme 'doom-tokyo-night t)
-
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Enable custom neotree theme (nerd-icons must be installed!)
@@ -20,9 +16,31 @@
 
 ;; Minimal Themes
 (use-package minimal-theme
-  :straight (minimal-theme :type git :host github :repo "ashwinjadhav818/emacs-minimal-theme")
-  :config
-  (load-theme 'minimal-black t))
+  :straight (minimal-theme :type git :host github :repo "shadowash8/emacs-minimal-theme"))
+
+;; Load theme based on window manager
+(defun get-current-wm ()
+  "Detect the current window manager."
+  (let ((desktop-env (getenv "XDG_CURRENT_DESKTOP"))
+        (session-type (getenv "DESKTOP_SESSION")))
+    (cond
+     ((or (string-match-p "hyprland" (or desktop-env ""))
+          (string-match-p "hyprland" (or session-type "")))
+      'hyprland)
+     ((or (string-match-p "dwm" (or desktop-env ""))
+          (string-match-p "dwm" (or session-type ""))
+          (and (executable-find "dwm") 
+               (string-match-p "dwm" (shell-command-to-string "ps -e | grep dwm"))))
+      'dwm)
+     (t 'unknown))))
+
+(load-file "~/.config/colors/minimal-matugen-theme.el")
+
+;; Load appropriate theme based on WM
+(pcase (get-current-wm)
+  ('hyprland (load-theme 'minimal-matugen t))
+  ('dwm (load-theme 'minimal-black t))
+  (_ (load-theme 'minimal-matugen t))) ;; Default to matugen if unknown
 
 (provide 'themes)
 ;;; themes.el ends here
